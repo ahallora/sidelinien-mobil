db6584a651b67c388d5050912c66af901aa4cf31
Anders Holm-Jensen <ah@allora.dk>
2022-10-02 11:41:19 +0200

    test content as json

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -11,7 +11,7 @@ export default async function TestEndpoint(
       "http://sidelinien.dk/forums/search.php?do=getnew&contenttype=vBForum_Event"; // hack to get to the vbform login
     await page.goto(loginPage);
 
-    res.send(await page.content());
+    res.json({ content: JSON.stringify(await page.content()) });
   } catch (e) {
     console.log(e);
     res.json({ error: e?.message ?? e });
