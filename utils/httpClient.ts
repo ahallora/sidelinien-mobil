fc0229226f09d0b5c9c3d66110bd1ae7c5ab5dce
Anders Holm-Jensen <ah@allora.dk>
2026-03-21 08:56:10 +0100

    fix: try without br zstd

--- utils/httpClient.ts
+++ utils/httpClient.ts
@@ -19,7 +19,7 @@ const BROWSER_HEADERS = {
   Accept:
     "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
   "Accept-Language": "da-DK,da;q=0.9,en-US;q=0.8,en;q=0.7",
-  "Accept-Encoding": "gzip, deflate, br, zstd",
+  "Accept-Encoding": "gzip, deflate",
   Connection: "keep-alive",
   "Upgrade-Insecure-Requests": "1",
   "Cache-Control": "max-age=0",
